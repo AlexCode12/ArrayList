6b0ac31c36be565784b163677e745f8b8e7eaf49
alexcode12 <alexander.olsson2@elev.ga.ntig.se>
2019-05-08 11:02:13 +0200

    First Commit. Psuedokod inkluderat

---
+++ src/arrayList.java
@@ -0,0 +1,19 @@
+/*
+   Psuedokod:
+   Fråga om användarens poäng
+   Lägg in det i en ArrayList
+   Sortera antalet
+    */
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class arrayList {
+    public static void main(String[] args) {
+        Scanner sn = new Scanner(System.in);
+        ArrayList <Integer> integerList;
+        integerList = new ArrayList<Integer>();
+        System.out.println("Skriv in ditt poäng här");
+
+    }
+}
