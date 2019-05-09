0f527eb0d8110c49ff30a4b95cb868993e67283b
alexcode12 <alexander.olsson2@elev.ga.ntig.se>
2019-05-09 15:34:02 +0200

    Det som kvarstår: Index + loop

--- src/arrayList.java
+++ src/arrayList.java
@@ -11,9 +11,28 @@ import java.util.Scanner;
 public class arrayList {
     public static void main(String[] args) {
         Scanner sn = new Scanner(System.in);
-        ArrayList <Integer> integerList;
-        integerList = new ArrayList<Integer>();
-        System.out.println("Skriv in ditt poäng här");
+        ArrayList <Integer> list = new ArrayList<Integer>();
+        int score;
+        do {
+            System.out.println("Skriv in ditt poäng här");
+            score = sn.nextInt();
 
+            if (score != -1) {
+            {
+                //ta reda på index.
+                    int index;
+                    //vart ska poängen vara
+                for (int i = 0; i < list.size(); i++) {
+                    /*
+                    Om score är större än det som finns, ska den vara på den raden.
+                     */
+                }
+                //list.add(index,score);
+                }
+            }
+        } while (score != -1);
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
     }
 }
