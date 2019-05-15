e30586cc50785b9d2ee3ed9a169d01726b9cd835
alexcode12 <alexander.olsson2@elev.ga.ntig.se>
2019-05-15 10:59:52 +0200

    Kommentarer + förbättringar i koden

--- src/arrayList.java
+++ src/arrayList.java
@@ -6,33 +6,41 @@
     */
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class arrayList {
     public static void main(String[] args) {
         Scanner sn = new Scanner(System.in);
-        ArrayList <Integer> list = new ArrayList<Integer>();
-        int score;
+        ArrayList<Integer> scores = new ArrayList<Integer>();
+        int t = 0;
+        System.out.println("How many objects should be sorted?");
+        int h = sn.nextInt();
         do {
-            System.out.println("Skriv in ditt poäng här");
-            score = sn.nextInt();
-
-            if (score != -1) {
-            {
-                //ta reda på index.
-                    int index;
-                    //vart ska poängen vara
-                for (int i = 0; i < list.size(); i++) {
-                    /*
-                    Om score är större än det som finns, ska den vara på den raden.
-                     */
-                }
-                //list.add(index,score);
-                }
+            System.out.println("\n Input next score");
+            int Score = sn.nextInt();
+            scores.add(Score);
+            System.out.println("This is the current list: ");
+            Collections.sort(scores, Collections.reverseOrder());
+            for (int i : scores) {
+                System.out.println(i);
             }
-        } while (score != -1);
-        for (int i = 0; i < list.size(); i++) {
-            System.out.println(list.get(i));
+            t++;
+        }while (t<h);
+        /*
+        Syftet med denna doWhile loop var att få till sorteringen efter att användaren har gett en input
+        på hur många scores som kommer att komma in, sedan sorteras det samt även en "Just nu är highscore x".
+        Sedan sorteras listan en gång till för att ge den slutgiltliga listan nedan.
+         */
+        Collections.sort(scores, Collections.reverseOrder());
+        System.out.println("This is the ordered list: ");
+        for (int i : scores) {
+            System.out.println(i);
         }
     }
 }
+
+/*
+Möjlig förbättring:
+Skulle kunna förbättras om det går att göra det mer smidigt med mindre kod, dock så funkar ju sorteringen.
+ */
